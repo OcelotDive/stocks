fa3f7e2f64e881b58150a69f749d6cdb8ef599fb
ocelotdive <oscar1dlh@hotmail.com>
2020-02-18 21:29:23 +0000

    add: news component

---
+++ src/app/components/spare/spare.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SpareComponent } from './spare.component';
+
+describe('SpareComponent', () => {
+  let component: SpareComponent;
+  let fixture: ComponentFixture<SpareComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SpareComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SpareComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
