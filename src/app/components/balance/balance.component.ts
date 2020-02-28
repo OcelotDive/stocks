6f54720ca86244dc57b385443e7d2fa77ed497da
ocelotdive <oscar1dlh@hotmail.com>
2020-02-28 13:38:27 +0000

    add: balance sheet component

--- src/app/components/balance/balance.component.ts
+++ src/app/components/balance/balance.component.ts
@@ -3,23 +3,23 @@ import { InvestorService } from '../../services/investor.service';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-income',
-  templateUrl: './income.component.html',
-  styleUrls: ['./income.component.css'],
-  providers: [InvestorService]
+  selector: 'app-balance',
+  templateUrl: './balance.component.html',
+  styleUrls: ['./balance.component.css']
 })
-export class IncomeComponent implements OnInit {
-  public IncomeStatement: Object[];
+export class BalanceComponent implements OnInit {
+  public balanceStatement: Object[];
   public companySymbol: string = "";
 
   constructor(public investorService: InvestorService, public route: ActivatedRoute) {
     this.scroll();
     this.route.params.subscribe(routeParams => {
     this.companySymbol = routeParams.symbolId.substring(1);
-    this.investorService.getAnnualIncome(this.companySymbol).subscribe((data: any) => {
+
+    this.investorService.getAnnualBalance(this.companySymbol).subscribe((data: any) => {
     
-      this.IncomeStatement = data.financials.slice(0,11).reverse();
-    
+      this.balanceStatement = data.financials.slice(0,11).reverse();
+      console.warn(this.balanceStatement)
     })
   })
    }
